9d3693ea7e9ce75548a7b54635e9c2bd77b79aa8
ud803 <ud803da@gmail.com>
2017-11-18 14:26:40 +0900

    committed at 2017년 11월 18일 오후 02시 26분 39초 KST

--- R/ggplot0-기초.R
+++
@@ -1,7 +0,0 @@
-## aes ##
-## Aesthetic mappings
-## aes(x, y, ...)
-# Aesthetics supplied to ggplot() are used as defaults for every layer
-# you can override them, or supply different aesthetics for each layer
-
-ggplot(mpg, aes(displ, hwy)) + geom_point()
